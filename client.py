fdea038c622c8d3849956c2c3774a587ac7b2af6
Pharoh0 <essameldinart@gmail.com>
2023-02-08 13:45:06 +0200

    Fix game

--- client.py
+++ client.py
@@ -5,10 +5,8 @@ from player import Player
 width = 500
 height = 500
 win = pygame.display.set_mode((width, height))
+pygame.init()
 pygame.display.set_caption("Client")
-
-clientNumber = 0
-
 
 
 def redrawWindow(win,player, player2):
@@ -21,14 +19,12 @@ def redrawWindow(win,player, player2):
 def main():
     run = True
     n = Network()
-
-
-
+    p = n.getP()
     clock = pygame.time.Clock()
 
     while run:
         clock.tick(60)
-
+        p2 = n.send(p)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
